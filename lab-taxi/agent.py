85c12e34a05c39cd8b9d67fa6d52e5e6b328608a
Tianzi Cai <tianzih@outlook.com>
2018-08-12 21:56:18 -0700

    Implemented expected Sarsa

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -12,6 +12,9 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.eps = 1.0
+        self.alpha = 0.1
+        self.N = 1
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,9 +27,11 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
+        if np.random.rand() > self.eps:
+            return np.argmax(self.Q[state])
         return np.random.choice(self.nA)
 
-    def step(self, state, action, reward, next_state, done):
+    def step(self, state, action, reward, next_state=None, done=False):
         """ Update the agent's knowledge, using the most recently sampled tuple.
 
         Params
@@ -37,4 +42,20 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        self.eps = min(0.0005, 1.0/self.N)
+        self.N += 1
+        if state in self.Q: 
+            Qsa = self.Q[state][action]
+        else: 
+            Qsa = 0.0
+            
+        policy_s = np.ones(self.nA) / self.nA * self.eps
+
+        if next_state in self.Q:
+            policy_s[np.argmax(self.Q[next_state])] = 1 - self.eps + (self.eps / self.nA)
+            EQsa_next = np.dot(self.Q[next_state], policy_s)
+        else:
+            EQsa_next = 0.0
+        
+        self.Q[state][action] = Qsa + self.alpha * (reward + EQsa_next - Qsa)
+        
