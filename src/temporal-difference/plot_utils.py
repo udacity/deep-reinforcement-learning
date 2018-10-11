6664a6c06ee7b14f8e8b9b2c3cd9b4596aa2ada7
CCThompson82 <christophercthompson82@gmail.com>
2018-10-11 17:41:39 +0100

    Chore: Run jupyter without token

--- src/temporal-difference/plot_utils.py
+++ src/temporal-difference/plot_utils.py
@@ -11,7 +11,7 @@ def plot_values(V):
 	ax = fig.add_subplot(111)
 	im = ax.imshow(V, cmap='cool')
 	for (j,i),label in np.ndenumerate(V):
-	    ax.text(i, j, np.round(label,3), ha='center', va='center', fontsize=14)
+		ax.text(i, j, np.round(label,3), ha='center', va='center', fontsize=14)
 	plt.tick_params(bottom='off', left='off', labelbottom='off', labelleft='off')
 	plt.title('State-Value Function')
 	plt.show()
