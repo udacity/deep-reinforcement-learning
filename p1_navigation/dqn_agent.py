a0686265157510f2934d0882b1970774898125c9
primeMover <herbert.danzinger@gmail.com>
2019-03-04 00:28:48 +0100

    update network size

--- p1_navigation/dqn_agent.py
+++ p1_navigation/dqn_agent.py
@@ -36,8 +36,8 @@ class Agent():
         self.seed = random.seed(seed)
 
         # Q-Network
-        self.qnetwork_local = QNetwork(state_size, action_size, seed, 64, 64).to(device)
-        self.qnetwork_target = QNetwork(state_size, action_size, seed, 64, 64).to(device)
+        self.qnetwork_local = QNetwork(state_size, action_size, seed, 128, 128).to(device)
+        self.qnetwork_target = QNetwork(state_size, action_size, seed, 128, 128).to(device)
         #self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
         self.optimizer = optim.RMSprop(self.qnetwork_local.parameters(), lr = LR)
 
