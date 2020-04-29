7213d4dc9a96c36bfc12a07604f2d2a61a900985
Tien-Thanh Nguyen <nttputus@gmail.com>
2020-04-29 12:11:54 +0200

    Update main.py to taxi-v3
    Fix taxi-v2 env to taxi-v3.
    New gym version will throw exception
    DeprecatedEnv: Env Taxi-v2 not found (valid versions include ['Taxi-v3'])

--- lab-taxi/main.py
+++ lab-taxi/main.py
@@ -3,6 +3,6 @@ from monitor import interact
 import gym
 import numpy as np
 
-env = gym.make('Taxi-v2')
+env = gym.make('Taxi-v3')
 agent = Agent()
 avg_rewards, best_avg_reward = interact(env, agent)
