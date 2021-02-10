2a530d2a9e3015fba2d84b30651b71e13bbb9a87
manualrg <manualrg@gmail.com>
2021-02-10 23:46:39 +0100

    added models and agents for reinforce

--- src/model.py
+++ src/model.py
@@ -112,3 +112,77 @@ class DuelQNetwork(nn.Module):
         x_out = x_val + (x_adv - x_adv_mean)
 
         return x_out
+
+
+class ReinforcePolicyConv(nn.Module):
+
+    def __init__(self, state_size, action_size, n_channels, seed):
+        super(ReinforcePolicyConv, self).__init__()
+        torch.manual_seed(seed)
+        self.seed = seed
+        self.state_size = state_size
+        self.action_size = action_size
+        self.n_channels = n_channels
+
+        self.conv1 = nn.Conv2d(n_channels, 4, kernel_size=6, stride=2, bias=False)
+        self.conv2 = nn.Conv2d(4, 16, kernel_size=6, stride=4)
+
+        s1 = get_out_dims_convs(inputsize=state_size, kernel_size=6, stride=2)
+        s2 = get_out_dims_convs(inputsize=s1, kernel_size=6, stride=4)
+        self.size = 16 * s2 * s2  # channels x w x l
+
+        # two fully connected layer
+        self.fc1 = nn.Linear(self.size, 256)
+        self.fc2 = nn.Linear(256, 1)
+
+        # Sigmoid to
+        self.sig = nn.Sigmoid()
+        self.softm = nn.Softmax()
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(-1, self.size)
+        x = F.relu(self.fc1(x))
+
+        if self.action_size == 2:
+            return self.sig(self.fc2(x))
+        else:
+            return self.softm(self.fc2(x))
+
+def get_out_dims_convs(inputsize, kernel_size, stride):
+    return round((inputsize - kernel_size + stride) / stride)
+
+
+class ReinforcePolicy(nn.Module):
+
+    def __init__(self, state_size, action_size, n_channels, seed):
+        super(ReinforcePolicyConv, self).__init__()
+        torch.manual_seed(seed)
+        self.seed = seed
+        self.state_size = state_size
+        self.action_size = action_size
+
+        # two fully connected layer
+        hidden_units = 256
+        self.fc1 = nn.Linear(self.state_size, hidden_units)
+        if self.action_size == 2:
+            self.fc2 = nn.Linear(hidden_units, 1)
+        else:
+            self.fc2 = nn.Linear(hidden_units, self.action_size)
+
+        # Sigmoid to
+        self.sig = nn.Sigmoid()
+        self.softm = nn.Softmax()
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(-1, self.size)
+        x = F.relu(self.fc1(x))
+
+        if self.action_size == 2:
+            return self.sig(self.fc2(x))
+        else:
+            return self.softm(self.fc2(x))
+
